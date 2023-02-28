51f290ed99dc3010ba9d63cd3a042091c4c445ba
Alex Soudant <asoudant@hotmail.fr>
2023-02-28 16:43:35 +0100

    [JAVA] anonymous class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 import java.io.File;
+import java.io.FileFilter;
 
 public class Main
 {
     public static void main(String[] args) {
-        JavaFileFilter fileFilter = new JavaFileFilter();
+        FileFilter fileFilter = new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.getName().endsWith(".java");
+            }
+        };
         File dir = new File("C:\\Users\\Alex\\Documents");
         File[] javaFiles = dir.listFiles(fileFilter);
         System.out.println("Welcome to Online IDE!! Happy Coding :)" + javaFiles);
