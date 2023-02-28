aead9ab809fa024242098ede1fa252ce55ee6ba5
Alex Soudant <asoudant@hotmail.fr>
2023-02-28 16:30:03 +0100

    [JAVA] demo start init project

---
+++ src/main/java/JavaFileFilter.java
@@ -0,0 +1,8 @@
+import java.io.File;
+import java.io.FileFilter;
+
+public class JavaFileFilter implements FileFilter {
+    public boolean accept(File file) {
+        return file.getName().endsWith(".pdf");
+    }
+}
