01115969dfa18dc945673d3c5a29834605f5680d
Alex Soudant <asoudant@hotmail.fr>
2023-02-28 17:03:43 +0100

    [JAVA] lambda runnable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,10 @@ import java.io.FileFilter;
 public class Main
 {
     public static void main(String[] args) {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0 ; i < 3 ; i++) {
-                    System.out.println("Hello from Thread [" + Thread.currentThread().getName() + "]");
-                }
-            };
+        Runnable runnable = () -> {
+            for (int i = 0 ; i < 3 ; i++) {
+                System.out.println("Hello from Thread [" + Thread.currentThread().getName() + "]");
+            }
         };
         Thread t = new Thread(runnable);
         t.start();
