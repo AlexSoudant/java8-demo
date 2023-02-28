0a7314c143c6fdb9a57042855d8fd0ee12f50e89
Alex Soudant <asoudant@hotmail.fr>
2023-02-28 17:09:57 +0100

    [JAVA] comparator lambda

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,25 @@
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main
 {
     public static void main(String[] args) {
-        Runnable runnable = () -> {
-            for (int i = 0 ; i < 3 ; i++) {
-                System.out.println("Hello from Thread [" + Thread.currentThread().getName() + "]");
+        Comparator<String> comp = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.compare(o1.length(), o2.length());
             }
         };
-        Thread t = new Thread(runnable);
-        t.start();
-        try {
-            t.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        Comparator<String> compLambda = (String o1, String o2) -> Integer.compare(o1.length(), o2.length());
+
+        List<String> list = Arrays.asList("***", "**", "****", "*");
+        Collections.sort(list, compLambda);
+
+        list.forEach(str -> System.out.println(str));
     }
 }
