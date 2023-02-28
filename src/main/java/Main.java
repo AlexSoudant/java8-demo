5861a82c34f85e6e8af254c01191b9ca45416931
Alex Soudant <asoudant@hotmail.fr>
2023-02-28 16:52:02 +0100

    [JAVA] first lambda

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,7 @@ import java.io.FileFilter;
 public class Main
 {
     public static void main(String[] args) {
-        FileFilter fileFilter = new FileFilter() {
-            @Override
-            public boolean accept(File file) {
-                return file.getName().endsWith(".java");
-            }
-        };
+        FileFilter fileFilter = (File file) -> file.getName().endsWith(".java");
         File dir = new File("C:\\Users\\Alex\\Documents");
         File[] javaFiles = dir.listFiles(fileFilter);
         System.out.println("Welcome to Online IDE!! Happy Coding :)" + javaFiles);
