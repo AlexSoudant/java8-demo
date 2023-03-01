3c36a904921faf52bbcb5ea482531975cea2bfec
Alex Soudant <asoudant@hotmail.fr>
2023-03-01 10:56:04 +0100

    [JAVA] patterns with predicate and consumer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,39 @@
 import java.io.File;
 import java.io.FileFilter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main
 {
     public static void main(String[] args) {
-        Comparator<String> comp = new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return Integer.compare(o1.length(), o2.length());
-            }
-        };
+        Predicate<String> p1 = s -> s.length() < 20;
+        Predicate<String> p2 = s -> s.length() > 10;
 
-        Comparator<String> compLambda = (String o1, String o2) -> Integer.compare(o1.length(), o2.length());
+        Predicate<String> p3 = p1.and(p2);
 
-        List<String> list = Arrays.asList("***", "**", "****", "*");
-        Collections.sort(list, compLambda);
+        List<String> noms = new ArrayList<>(Arrays.asList("Georges", "Bernard", "Henry", "Bartholomé Naomie"));
+        // Java8 : Stream.of("Georges", "Bernard", "Henry", "Bartholomé").collect(Collectors.toList());
 
-        list.forEach(str -> System.out.println(str));
+        List<String> filteredNamesByLength = noms.stream().filter(p3).collect(Collectors.toList());
+        Consumer<String> printCons = System.out::println;
+        filteredNamesByLength.forEach(printCons);
+
+        System.out.println("-------------------- ");
+
+        String target = "Georges";
+        Predicate<String> id = Predicate.isEqual(target);
+        List<String> filteredNamesById = noms.stream().filter(id).collect(Collectors.toList());
+        filteredNamesById.forEach(printCons);
+
+        System.out.println("-------------------- ");
+
+        List<String> result = new ArrayList<>();
+        Consumer<String> addToResult = result::add;
+
+        noms.forEach(printCons.andThen(addToResult));
+        System.out.println("size of result = " + result.size());
     }
 }
