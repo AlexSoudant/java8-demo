f4147e76bb53777a7cdc9e6adeedb9ce28697005
Alex Soudant <asoudant@hotmail.fr>
2023-03-01 16:58:44 +0100

    [JAVA] bonus

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,34 @@
-import java.io.FileFilter;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class Main
 {
     public static void main(String[] args) {
-        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
-        List<Integer> list2 = Arrays.asList(6, 7, 8);
-        List<Integer> list3 = Arrays.asList(9, 20, 30);
+        String s = "Hello world!";
+        IntStream stream = s.chars();
 
-        List<List<Integer>> list = Arrays.asList(list1, list2, list3);
+        stream.mapToObj(letter -> (char)letter)
+                .map(Character::toUpperCase)
+                .forEach(System.out::println);
 
-        System.out.println(list);
+        StringJoiner sj = new StringJoiner(", ", "{", "}");
+        sj.add("one").add("two");
+        String chain = sj.toString();
+        System.out.println(chain);
 
-        Function<List<Integer>, Stream<Integer>> flatmapper = Collection::stream;
+        //sort
+        List<String> strings = Arrays.asList("xavier", "alex", "cedric");
 
-        String sentence = list.stream()
-                .flatMap(flatmapper)
-                .filter(number -> number > 3)
-                .map(Object::toString)//number -> number.toString()
-                .collect(Collectors.joining(", ")); //toList()
+        strings.sort(Comparator.naturalOrder());
+        System.out.println(strings);
 
-        System.out.println(sentence);
-
-        Person user1 = new Person("Boris",20, 0);
-        Person user2 = new Person("Loic",216, 1);
-        Person user3 = new Person("Samantha",16, 2);
-
-        List<Person> users =  Arrays.asList(user1, user2, user3);
-
-        Map<Integer, Long> filteredUsers = users.stream()
-                .filter(user -> user.age < 100)
-                .collect(Collectors.groupingBy(Person::getAge, Collectors.counting()));
-
-        System.out.println(filteredUsers);
+        // Numbers
+        long max = Long.max(1L, 2L);
+        BinaryOperator<Long> sum = Long::sum;
     }
 }
