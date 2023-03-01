9686c24cf032982583ae90462b3b4f2e2012e8eb
Alex Soudant <asoudant@hotmail.fr>
2023-03-01 14:55:18 +0100

    [JAVA] flatmap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,26 @@
+import java.io.FileFilter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Main
 {
     public static void main(String[] args) {
-        Stream<String> stream = Stream.of("one", "two", "three");
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> list2 = Arrays.asList(6, 7, 8);
+        List<Integer> list3 = Arrays.asList(9, 20, 30);
 
-        Predicate<String> p1 = Predicate.isEqual("two");
-        Predicate<String> p2 = Predicate.isEqual("three");
+        List<List<Integer>> list = Arrays.asList(list1, list2, list3);
 
-        List<String> list = new ArrayList<>();
+        System.out.println(list);
 
-        stream
-                .peek(System.out::println)
-                .filter(p1.or(p2))
-                .forEach(list::add); //peek
+        Function<List, Integer> size = List::size;
+        Function<List<Integer>, Stream<Integer>> flatmapper = Collection::stream;
 
-        System.out.println("Done!");
-        System.out.println("size = " + list.size());
+        list.stream().flatMap(flatmapper).forEach(System.out::println);
     }
 }
